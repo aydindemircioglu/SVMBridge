a53061f9da23235812fc55326556a8a238c50b8c
aydin demircioglu <githubAD@cloned.de>
2015-01-15 15:17:29 +0100

    remove tmp

--- tmp/getSupportedInfillOptFunctions.R
+++
@@ -1,7 +0,0 @@
-#' Get names of supported infill-criteria optimizers.
-#'
-#' @return [\code{character}]
-#' @export
-getSupportedInfillOptFunctions = function() {
-  c("cmaes", "focussearch", "ea", "nsga2")
-}
--- tmp/getSupportedMultipointInfillOptFunctions.R
+++
@@ -1,7 +0,0 @@
-#' Get names of supported multipoint infill-criteria opimizers.
-#'
-#' @return [\code{character}]
-#' @export
-getSupportedMultipointInfillOptFunctions = function() {
-  c("cl", "lcb", "multicrit")
-}
