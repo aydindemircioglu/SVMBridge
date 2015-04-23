fbff66843f6c6f59fc76026e93aeb79d66c80550
aydin demircioglu <githubAD@cloned.de>
2015-04-23 12:00:38 +0200

    add Rcpp to project

---
+++ src/SparseData.cpp
@@ -0,0 +1,50 @@
+// -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+//
+// SVMBridge
+// 	-- readSparse
+
+
+//
+// Copyright (C) 2015  Aydin Demircioglu, aydin.demircioglu /at/ ini.rub.de
+//
+// This file is part of the SwarmSVM library for GNU R.
+// It is made available under the terms of the GNU General Public
+// License, version 2, or at your option, any later version,
+// incorporated herein by reference.
+//
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this program; if not, write to the Free
+// Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+// MA 02111-1307, USA
+
+#include <Rcpp.h>
+
+
+using namespace std;
+
+
+
+
+using namespace Rcpp;
+
+
+
+RcppExport SEXP readSparseData (SEXP svmParameters) {
+	
+	return R_NilValue;
+}
+
+
+
+
+RcppExport SEXP writeSparseData (SEXP x, SEXP y, SEXP svmParameters) {
+	
+	return R_NilValue;
+}
+
