85c92efd545c90b877e2a3ece666d06b7b32f150
aydin demircioglu <githubAD@cloned.de>
2015-06-02 11:14:51 +0200

    add test for subsampling

--- R/subsampleDataByCutoff.R
+++ R/subsampleDataByCutoff.R
@@ -9,11 +9,14 @@
 #'		between 0..1.0000000001, it will be interpreted as a fraction, e.g.
 #'		0.5 means subsampling half of the file. If the rate is above 1.0000000001,
 #'		the value is taken as the absolut number of data points, e.g. 50 will
-#'		subsample exactly 50 points (the first 50 of the file).
+#'		subsample exactly 50 points (the first 50 of the file). If anything above 1.00..1
+#'		is given, this number will be floored first, so 50.9 lines will yield 50 lines.
 #'	
 #' @note		if the subsamplingRate == 1 then no copy is done, but the
-#' file itself is returned. 
-#'
+#' 	file itself is returned. 
+#'	@note		a temporary file will be created (in case any subsampling has to be
+#'		done)-- this will stay until the R session finishes.
+#'	@note		there is no way to subsample just ONE line from the file.
 #' @export
 #'
 subsampleDataByCutoff <- function ( filepath = "", subsamplingRate = -1)
@@ -28,8 +31,6 @@ subsampleDataByCutoff <- function ( filepath = "", subsamplingRate = -1)
 	subsampledFile = filepath
 	if (subsamplingRate > 0.0) 
 	{
-		# create subsampled file on /tmp
-		subsampledFile = tempfile()
 		originalFile = filepath
 
 		# in case of subsamplingRate == 1.0 we do not do anything
@@ -45,6 +46,12 @@ subsampleDataByCutoff <- function ( filepath = "", subsamplingRate = -1)
 			nLines = R.utils::countLines (filepath)[1] 
 			subsamplingSize = floor (nLines*subsamplingRate)
 		}
+
+		# make sure whatever we have is integer
+		subsamplingSize = floor (subsamplingSize)
+		
+		# create subsampled file on /tmp
+		subsampledFile = tempfile(pattern = paste("sub", subsamplingSize, "", sep = "_"))
 
 		# subsample by read-writing, unsure if this is performant enough
 		srcConn = file (originalFile)
---
+++ tests/testthat/test_subsampleDataByCutoff.R
@@ -0,0 +1,49 @@
+context("subsampleDataByCutoff")
+
+
+test_that("subsampling with specified rate does work", {
+	# create a sparse file with 1000 lines
+	X = data.frame (f1 = runif(1000), f2 = runif(1000), f3 = runif(1000))
+	Y = data.frame (Y = sign (runif(1000) - 0.5))
+	orgData = tempfile()
+	writeSparseData(orgData, as.matrix(X), as.matrix(Y), verbose = TRUE, zeroBased = FALSE)
+	
+	# now subsample the file
+	for (i in seq(1,100)) {
+		outputFile = subsampleDataByCutoff (orgData, i/100)
+		lines = R.utils::countLines (outputFile)[1]
+		expect_equal (i*10, lines)
+	}
+})
+
+
+test_that("subsampling with specified number of lines does work", {
+	# create a sparse file with 1000 lines
+	X = data.frame (f1 = runif(1000), f2 = runif(1000), f3 = runif(1000))
+	Y = data.frame (Y = sign (runif(1000) - 0.5))
+	orgData = tempfile()
+	writeSparseData(orgData, as.matrix(X), as.matrix(Y), verbose = TRUE, zeroBased = FALSE)
+	
+	# now subsample the file
+	for (i in seq(2,999,9)) {
+		outputFile = subsampleDataByCutoff (orgData, i)
+		lines = R.utils::countLines (outputFile)[1]
+		expect_equal (i, lines)
+	}
+})
+
+
+test_that("parameters are checked", {
+	orgData = tempfile()
+	expect_error (subsampleDataByCutoff (""))
+	expect_error (subsampleDataByCutoff ("", 44))
+	expect_error (subsampleDataByCutoff (orgData, -55))
+})
+
+
+test_that("handling with strange files works", {
+	orgData = tempfile()
+	# check what happens if file has no lines
+	# too many lines etc
+})
+
