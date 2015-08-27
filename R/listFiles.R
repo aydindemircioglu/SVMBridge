fd5ae8ff0d6d8694c81d5ada4757384270f887fc
aydin demircioglu <githubAD@cloned.de>
2015-08-27 12:04:55 +0200

    revert lido clustering stop

--- R/listFiles.R
+++ R/listFiles.R
@@ -4,8 +4,8 @@
 #' 
 #' @export
 listFiles = function (...) {
-	if (is.null(grep("lidong", (Sys.info()["nodename"]))) == FALSE) {
-		stop ("Sorry, on LIDO we do not use this list.files function.")
-	}
+# 	if (is.null(grep("lidong", (Sys.info()["nodename"]))) == TRUE) {
+# 		stop ("Sorry, on LIDO we do not use this list.files function.")
+# 	}
 	list.files (...)
 }
