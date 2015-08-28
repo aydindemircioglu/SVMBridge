9867a043d2041190b5416e060eff54e31349ab14
Hanna <Hanna.Houphouet@rub.de>
2015-08-28 11:47:34 +0200

    fix

--- R/expandTilde.R
+++ R/expandTilde.R
@@ -3,11 +3,7 @@
 # SVMBridge 
 #		(C) 2015, by Aydin Demircioglu
 #
-<<<<<<< HEAD
 #		expandTilde.R
-=======
-#		findSVMSoftware.R
->>>>>>> 31ed93e5280ab470fc98db2e90d03e1e4bf5bde6
 # 
 # SVMBridge is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
@@ -22,38 +18,8 @@
 # Please do not use this software to destroy or spy on people, environment or things.
 # All negative use is prohibited.
 #
-<<<<<<< HEAD
 
-#' expandTilde
-#'		expand tilde character to the home directory of the current user
-#'
-#' @param	path	 	a certain path
-#' @param	verbose		print messages while searching?
-#' @return	path with expanded tilde character
-
-expandTilde <- function(path = NULL, verbose = FALSE){
-		#look for tilde characters and expand them
-		if(grepl("~", path) == TRUE){
-			if(.Platform$OS.type == "windows"){
-				firstPart = path.expand("~")
-				firstPart = substr(firstPart, 1, nchar(firstPart) - 10)
-				secondPart = substr(path, 2, nchar(path))
-				path = paste(firstPart, secondPart, sep="")
-			}
-			else
-				path = path.expand(path)
-			path = gsub("[\\]", "/", path)
-			
-			if(verbose == TRUE){
-				BBmisc::messagef("  Expanded path: %s", path)
-			}
-		}
-		return (path)
-	}
-=======
  
-
-
 #' Expand path with tilde 
 #'	
 #' Given a path, try to expand the path, crossplatform independent.
@@ -82,4 +48,3 @@ expandTilde <- function(path = NULL, verbose = FALSE){
 	return (path)
 }
 
->>>>>>> 31ed93e5280ab470fc98db2e90d03e1e4bf5bde6
