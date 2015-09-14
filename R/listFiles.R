b5d66214fdfec8fd5b29d39d48146655093262c1
aydin demircioglu <githubAD@cloned.de>
2015-09-14 14:16:12 +0200

    stupidest thing eveR

--- R/listFiles.R
+++ R/listFiles.R
@@ -3,7 +3,7 @@
 #' 
 #' @export
 listFiles = function (...) {
-	if (is.null(grep("lidong", (Sys.info()["nodename"]))) == FALSE) {
+	if (is.null(grep("lidong", (Sys.info()["nodename"]))) == TRUE) {
 		stop ("Sorry, on LIDO we do not use this list.files function.")
 	}
 	list.files (...)
